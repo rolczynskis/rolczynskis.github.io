79f06fc436114f5c026df16d209ccf8f20140d18
Lisa Rolczynski <lisa.sano@gmail.com>
2024-02-23 17:11:26 -0800

    minify css

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("assets/images");
   eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
   return {
     dir: {
